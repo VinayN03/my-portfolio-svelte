a3c1737259ba48ce88b6eabd85b7211aaee6cf93
VinayN03 <vinaykumar.namadi@gmail.com>
2025-01-04 16:40:22 -0600

    Hero Section

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,15 @@
+<section class="hero-section default-margin">
+    <h3>Hello! I'm Vinay</h3>
+    <div class="underscore mb-m"></div>
+    <h2>I'm a Python developer with expertise in Artificial Intelligence, Machine Learning, 
+        Deep Learning and 
+    </h2>
+    <h2 class="dark-grey">Natural Language Processing</h2>
+    <button>Let's talk!</button>
+</section>
+
+<style>
+    .hero-section {
+        padding-top: 60px;
+    }
+</style>
