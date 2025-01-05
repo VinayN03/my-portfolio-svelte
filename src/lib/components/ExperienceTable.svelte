a085e460cf10a0023fdbc7e2029e37f0cb8f58c6
VinayN03 <vinaykumar.namadi@gmail.com>
2025-01-05 05:08:17 -0600

    Work Experience Table Styling

--- src/lib/components/ExperienceTable.svelte
+++ src/lib/components/ExperienceTable.svelte
@@ -9,19 +9,19 @@
     const workExperience: WorkExperience[] = [
         {
             jobTitle: "Software Developer",
-            company: "Emagia Corporation",
+            company: "Emagia Corporation - Lewisville, TX, USA",
             startDate: "2024-07",
         },
         {
             jobTitle: "Sr. Software Engineer",
-            company: "Emagia Corporation",
+            company: "Emagia Corporation - Hyderabad, INDIA",
             startDate: "2018-10",
             endDate: "2024-07"
         },
 
         {
             jobTitle: "GIS Engineer",
-            company: "RMSI Pvt Limited",
+            company: "RMSI Pvt Limited - Hyderabad, INDIA",
             startDate: "2018-10",
             endDate: "2024-07"
         },
@@ -51,5 +51,42 @@
         {/each}
 
     </ul>
-    <h3 class="headline">Past work <br /> Experience</h3>
+    <h3 class="headline">Work <br /> Experience</h3>
 </section>
+
+<style>
+    .work-experience {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+    }
+
+    .work-experience-list {
+        width: 50%;
+    }
+
+    .headline {
+        text-align: right;
+        margin-top: 90px;
+        margin-right: 120px;
+        
+    }
+
+    .work-item {
+        border-bottom: 1px solid #f0eded;
+        padding-bottom: 12px;
+    }
+
+    .work-item:not(:first-of-type) {
+        padding-top: 16px;
+    }
+
+    .work-item p {
+        margin-bottom: 0;
+    }
+
+    .company-and-date {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
