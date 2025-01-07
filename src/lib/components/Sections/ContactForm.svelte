b394ddb563d76c59e62501506ce00f830c7d2fcb
VinayN03 <vinaykumar.namadi@gmail.com>
2025-01-07 15:30:00 -0600

    Contact Me Section Responsiveness

--- src/lib/components/Sections/ContactForm.svelte
+++ src/lib/components/Sections/ContactForm.svelte
@@ -3,12 +3,14 @@
 </script>
 
 <section class="contact-section mt-l">
-    <SectionHeadline sectionName="contact-form">Contact Me</SectionHeadline>
+    <div class="contact-div">
+    <SectionHeadline sectionName={"contact-me"}>Contact Me</SectionHeadline>
 
     <div class="default-margin mt-m contact-details dark-grey">
     <p> vinaykumar.namadi@gmail.com </p>
     <p>+1 945 267 7514</p>
     <p>Plano, Texas, 75024 </p>
+</div>
 </div>
 </section>
 
@@ -16,24 +18,31 @@
     .contact-section {
         background-color: #fbfbfb;
         padding: 30px 0px;
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        margin: 0 auto;
     }
 
-    .contact-details {
-        padding-top: 60px;
+    .contact-div {
+        width: 100%;
+
+    }
+
+     .contact-details {
+        padding-top: 30px;
+        display: block;
+        text-align: center;
         
     }
-
     .contact-details p{
-        text-align: center;
-        margin: -5px 0px;
-        font-size: 24px;
-        font-weight: 100;
-        line-height: 1.5;
-        letter-spacing: 0px;
+        padding: 0;
+        margin: 0;
+        font-size: 18px;
     }
 
     .contact-details p:last-of-type{
         margin-bottom: 100px;
-    }
+    } 
     
 </style>
